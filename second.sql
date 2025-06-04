2a584360acc09a50c350af5a9eb7fb65e2d8cbd9
HipeoQA <decent1225@gmail.com>
2025-06-04 04:05:47 +0300

    Update second.sql

--- second.sql
+++ second.sql
@@ -2,7 +2,7 @@ SELECT track,
     CASE 
         WHEN finished == true THEN 2
         WHEN canсelled == true THEN -1
-        WHEN inDelivery == true THEN 1
+        WHEN "inDelivery" == true THEN 1
         ELSE 0
     END
 FROM "Orders"
